4c1ad8c9ab37d6639a1978a623182b7876d11d04
Superamaja <connorlin06@gmail.com>
2023-06-20 22:18:57 +0800

    Api working for no context

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,7 +1,28 @@
-from django.http import HttpResponse
+import openai
+from django.http import JsonResponse, StreamingHttpResponse
+from rest_framework.decorators import api_view
+
 from .settings import OPENAI_KEY
 
 # Create your views here.
 
+openai.api_key = OPENAI_KEY
+
+
+@api_view(["POST"])
 def chat(request):
-    return HttpResponse("Hello, world. Your OpenAI key is: " + OPENAI_KEY)
+    try:
+        prompt = request.data["prompt"]
+    except KeyError:
+        return JsonResponse({"error": "No prompt provided."}, status=400)
+
+    def generate_response():
+        for resp in openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}], stream=True
+        ):
+            content = resp["choices"][0]["delta"]
+
+            if content is not None and "content" in content:
+                yield content["content"]
+
+    return StreamingHttpResponse(generate_response(), content_type="text/event-stream")
